4020e8aae1e5548317fe389fade60196468eaf94
GuillermoACP <guillermoacp93@gmail.com>
2025-08-22 17:45:49 -0600

    Interacion Incompleto

---
+++ 05_Iteracion/Ejercicio_01.py
@@ -0,0 +1,29 @@
+'''
+Ejercicio 1: Escribe un programa que lea repetidamente números hasta que el usuario introduzca “fin”. Una vez se haya introducido “fin”, muestra por pantalla el total, la cantidad de números y la media de esos números. Si el usuario introduce cualquier otra cosa que no sea un número, detecta su fallo usando try y except, muestra un mensaje de error y pasa al número siguiente.
+'''
+
+def contador(numeros):
+    while True:
+        suma = 0.0
+        num_total = 0
+        return numeros
+    
+
+
+num = float(input('Introduce un numero: '))
+x = contador(num)
+print(x)
+
+
+
+
+
+'''
+Introduzca un número: 4
+Introduzca un número: 5
+Introduzca un número: dato erróneo
+Entrada inválida
+Introduzca un número: 7
+Introduzca un número: fin
+16 3 5.33333333333
+'''
