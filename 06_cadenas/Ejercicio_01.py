442c57eabb04dbb50a27ecbc5a251cbbd71c6c77
GuillermoACP <guillermoacp93@gmail.com>
2025-08-24 23:40:32 -0600

    corregido

--- 06_cadenas/Ejercicio_01.py
+++ 06_cadenas/Ejercicio_01.py
@@ -1,9 +1,17 @@
 '''
 Ejercicio 1: Escribe un bucle while que comience con el último carácter en la cadena y haga un recorrido hacia atrás hasta el primer carácter en la cadena, imprimiendo cada letra en una línea independiente.
 '''
-fruta = 'banana'
-ultima_letra = len(fruta) - 1
-while ultima_letra >= 0:
-    letra = fruta[ultima_letra]
-    print(letra)
-    ultima_letra = ultima_letra - 1
+
+def hacia_atras():
+    while True:
+        palabra = input ('Escribe una palabra o FIN para terminar:')
+        if palabra.lower() == "fin":
+            break
+        ultima_letra = len(palabra) -1
+        while ultima_letra >= 0:
+            letra = palabra[ultima_letra]
+            print(letra)
+            ultima_letra = ultima_letra - 1
+            
+        
+hacia_atras()
